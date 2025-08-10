435d495865d62becb183eff8a400c679f158a11f
Josephine Emenya <115475933+josieemenya@users.noreply.github.com>
2025-08-10 02:20:20 +0100

    i have made a text box, i have added an input where if you give the item to mean ghost the hint text appears

--- scenes/ghost.gd
+++ scenes/ghost.gd
@@ -4,17 +4,26 @@ extends Sprite2D
 var base_pos=300;
 var time_duration = 0.0;
 var items = {}
+var min_distance = 100
+var text
+
+
+func create_textbox()->void:
+	
+	pass
+
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	var base_pos = self.position.y
 	var item_container = $"../CanvasLayer/ItemArea/HBoxContainer"
+	text = $"../TextBox"
 	if (item_container != null):
 		items =  item_container.get_children()
 	else :
 		print("nullptr")	
-	
+	text.visible = false; 
 	pass # Replace with function body.
 
 
@@ -22,9 +31,18 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	time_duration += delta
 	self.position.y = base_pos + sin(time_duration * 5) * 30
+	for x in items : 
+		if (x.position == self.position) :
+			print("item is overlapping with ghost")
 	pass
 
 
 func _input(event) -> void:
-	
-	pass
+		var cases; 
+		if (event.is_action_released("get_hint")) : #and is holding item
+			if text.visible == true :
+				text.visible = false
+			if (items[0].dragging && items[0].global_position.distance_to(self.global_position) < min_distance) :
+				text.visible = true
+			
+pass
